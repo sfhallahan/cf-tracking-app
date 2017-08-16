ae891dee9c07e9a6983235ff66d578dff8669b5d
seanhallahan <sean.hallahan@accenturefederal.com>
2017-08-16 16:47:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+Boilerplate set up to include react, react-redux and react-router-dom. Set up the basic components and file structure that I use for my projects.
---
+++ src/containers/Root.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import App from './App';
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  </Provider>
+);
+
+//Root.propTypes = {
+//  store: PropTypes.object.isRequired,
+//};
+
+export default Root;
