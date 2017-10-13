41873c6008a02eaea836c3fefb1eec608bac9cde
Sean <sfhallahan@gmail.com>
2017-10-13 17:31:07 -0400

    pushing changes to move to other comp

--- src/components/Log/Log.js
+++ src/components/Log/Log.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { logContainer } from './styles.css'
 
 export default function Log(props) {
   return (
-    <p> loggy log log</p>
+    <div className={logContainer}>
+      <p>{'This new workspace is coo'}</p>
+      <p>{'Hell yeah it is... Definitely going to take some getting used to'}</p>
+      <h4>{'I think it will really just be the keyboard'}</h4>
+    </div>
   )
 }
